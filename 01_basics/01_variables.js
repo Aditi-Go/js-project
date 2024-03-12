e2da06fcd5e69b5b29375e27d7338e12b4535399
Aditi-Go <162990639+Aditi-Go@users.noreply.github.com>
2024-03-12 13:19:58 +0000

    Variable Discussion

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,19 @@
+const accountId = 144553
+let accountEmail = "aditi@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountstate;
 
+// account = 2 // Not allowed 
+
+accountEmail = "aditi.com"
+accountPassword = "452136987"
+accountCity = "Bikaner"
+
+console.log(accountId);
+
+/*
+PREFER NOT TO USE VAR 
+BECAUSE OF ISSUE IN BLOCK SCOPE AND FUNCTIONAL SCOPE
+*/
+console.table([accountEmail, accountPassword, accountId, accountCity, accountstate])
