a877add5504dc445e34a7bded4d5b6aaa9f3e739
Aditi-Go <162990639+Aditi-Go@users.noreply.github.com>
2024-03-12 13:20:24 +0000

    Datatype Discussion

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // We are using nodejs not browser 
+console.log(3+3);
+console.log("Aditi")
+
+
+
+let name ="aditi"
+let age = 32
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null =. standalone value 
+// Undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // null is an object
+console.log(typeof undefined); // undefined
