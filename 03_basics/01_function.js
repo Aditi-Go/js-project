db6d6705623d6e46b7cbaf03f1703f88f18c394f
Aditi-Go <162990639+Aditi-Go@users.noreply.github.com>
2024-04-26 04:44:21 +0000

    Functions - Done

---
+++ 03_basics/01_function.js
@@ -0,0 +1,23 @@
+// console.log("A");
+// console.log("d");
+// console.log("i");
+// console.log("t");
+// console.log("i");
+
+function sayMyName() {
+    console.log("A");
+console.log("d");
+console.log("i");
+console.log("t");
+console.log("i");
+
+}
+sayMyName()
+
+function addTwoNumbers(number1, number2) {
+    console.log(number1 + number2);
+}
+
+const result = addTwoNumbers(3, 5)
+
+console.log("Result: ", result);
